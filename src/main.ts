18f26d630d525b0cfc2bb6323b6d7eb7c5da1c30
Antoine Romand <antoine.romand@epitech.eu>
2026-02-07 19:39:34 +0100

    Create footer and fix some css issues.

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,16 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppHeader } from './app/components/app-header/app-header';
 import { provideRouter, RouterOutlet } from '@angular/router';
 import { routes } from './app.routes';
+import { AppFooter } from './app/components/app-footer/app-footer';
 
 @Component({
 	selector: 'app-root',
 	template: `
 	<app-header [isAuthenticated]="isAuthenticated()"></app-header>
 	<router-outlet></router-outlet>
+	<app-footer></app-footer>
   `,
-	imports: [RouterOutlet, AppHeader],
+	imports: [RouterOutlet, AppHeader, AppFooter],
 })
 export class App {
 	name = 'Angular';
