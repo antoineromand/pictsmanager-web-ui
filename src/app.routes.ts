71e8ee7babf58d310bd694f85f386772fe242ed0
Antoine Romand <antoine.romand@epitech.eu>
2026-02-07 17:48:08 +0100

    add router and create home page component.

---
+++ src/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { HomePage } from './app/pages/home-page/home-page';
+
+export const routes: Routes = [
+    { path: "", component: HomePage }
+];
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppHeader } from './app/components/app-header/app-header';
+import { provideRouter, RouterOutlet } from '@angular/router';
+import { routes } from './app.routes';
 
 @Component({
 	selector: 'app-root',
 	template: `
-	<app-header [isAuthenticated]="isAuthenticated()"><app-header>
+	<app-header [isAuthenticated]="isAuthenticated()"></app-header>
+	<router-outlet></router-outlet>
   `,
-	imports: [AppHeader],
+	imports: [RouterOutlet, AppHeader],
 })
 export class App {
 	name = 'Angular';
 	isAuthenticated = signal(false);
 }
 
-bootstrapApplication(App);
+bootstrapApplication(App, {
+	providers: [provideRouter(routes)]
+});
