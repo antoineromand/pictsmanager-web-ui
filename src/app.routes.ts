321270e2429ca661150ff4aede29eb2c7c92582c
Antoine Romand <antoine.romand@epitech.eu>
2026-02-08 00:14:37 +0100

    Create a about page (about me and the project).

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './app/pages/home-page/home-page';
 import { LoginPage } from './app/pages/authentication/login-page/login-page';
+import { AboutMe } from './app/pages/about-me/about-me';
 
 export const routes: Routes = [
     { path: "", component: HomePage },
-    { path: "login", component: LoginPage }
+    { path: "login", component: LoginPage },
+    { path: "about", component: AboutMe }
 ];
---
+++ src/app/pages/about-me/about-me.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about-me',
+  imports: [],
+  templateUrl: './about-me.html'
+})
+export class AboutMe {
+
+}
